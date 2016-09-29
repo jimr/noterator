388ab71f528aed69432d196df80c17a9fbedb2c0
James Rutherford <jim@jimr.org>
2016-09-29 23:12:57 +0100

    why test on a Mac when you can pretend you're testing on a Mac?

--- tests/test_desktop_plugin.py
+++ tests/test_desktop_plugin.py
@@ -4,10 +4,12 @@ import mock
 import unittest
 
 from noterator import Noterator, DESKTOP
+from noterator.plugins.desktop import Platforms
 
 
 class TestDesktopPlugin(unittest.TestCase):
     @mock.patch('noterator.plugins.desktop.objc')
+    @mock.patch('noterator.plugins.desktop.PLATFORM', new=Platforms.MAC)
     def test_desktop_plugin_mac(self, objc):
         nc = mock.Mock()
         cls1, cls2 = mock.Mock(), mock.Mock()
